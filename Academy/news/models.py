26238531b584d2bfb76bb6a2c27c6e7e245c2386
1152761042@qq.com <1152761042@qq.com>
2015-05-12 20:56:30 +0800

    I just chang the news part of README.md

--- Academy/news/models.py
+++ Academy/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class News(models.Model):
+    date = models.DateField(auto_now_add=True)
+    title = models.CharField( max_length=130)
+
+
+class NewsComment(models.Model):
+    date = models.DateField(auto_now_add=True)
+    reviewer = models.ForeignKey(User,related_name='reviewer')
+    content = models.TextField()
+
